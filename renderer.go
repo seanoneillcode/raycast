31ec5c5b8a8ede4257bceea3077409596b101f15
Sean ONeill <seansaysmeh@gmail.com>
2022-07-31 11:36:46 +0100

    minor rendering cleanup

--- renderer.go
+++ renderer.go
@@ -1,11 +1,9 @@
 package raycast
 
 import (
+	"github.com/hajimehoshi/ebiten/v2"
 	"image"
 	"image/color"
-	"math"
-
-	"github.com/hajimehoshi/ebiten/v2"
 )
 
 type Renderer struct {
@@ -39,7 +37,7 @@ func (r *Renderer) Render(screen *ebiten.Image, w *World) {
 
 func (r *Renderer) drawRay(ray ray, index int) {
 
-	lineHeight := (int)(math.Round(ScreenHeight / ray.distance))
+	lineHeight := (int)(ScreenHeight / ray.distance)
 
 	//calculate lowest and highest pixel to fill in current stripe
 	drawStart := ScreenHeight/2 - lineHeight/2
@@ -51,41 +49,39 @@ func (r *Renderer) drawRay(ray ray, index int) {
 		drawEnd = ScreenHeight - 1
 	}
 
-	width := ScreenWidth / NumRays
-	var textureX = int(ray.wallx * TextureWidth)
-	// flip textures if looking in opposite direction
+	var texX = int(ray.wallx * TextureWidth)
+	//flip textures if looking in opposite direction
 	if ray.side == 0 && ray.dir.x > 0 {
-		textureX = TextureWidth - textureX - 1
+		texX = TextureWidth - texX - 1
 	}
 	if ray.side == 1 && ray.dir.y < 0 {
-		textureX = TextureWidth - textureX - 1
+		texX = TextureWidth - texX - 1
 	}
 
+	x := index
 	step := float64(TextureHeight) / float64(lineHeight)
 	texPos := float64(drawStart-ScreenHeight/2+lineHeight/2) * step
 
-	for x := width * index; x < width*(index+1); x++ {
-		for y := 0; y < ScreenHeight; y++ {
-			if y > drawStart && y < drawEnd {
+	for y := 0; y < ScreenHeight; y++ {
+		if y > drawStart && y < drawEnd {
 
-				texY := int(texPos) & (TextureHeight - 1)
-				texPos += step
-				img := r.textures[0]
-				c := img.At(textureX, texY)
-				if ray.side == 0 {
-					rgba := color.RGBAModel.Convert(c).(color.RGBA)
-					rgba.R = rgba.R / 2
-					rgba.G = rgba.G / 2
-					rgba.B = rgba.B / 2
-					c = rgba
-				}
-				r.SetActualPixel(float64(x), float64(y), c)
+			texY := int(texPos) & (TextureHeight - 1)
+			texPos += step
+			img := r.textures[0]
+			c := img.At(texX, texY)
+			if ray.side == 0 {
+				rgba := color.RGBAModel.Convert(c).(color.RGBA)
+				rgba.R = rgba.R / 2
+				rgba.G = rgba.G / 2
+				rgba.B = rgba.B / 2
+				c = rgba
+			}
+			r.SetActualPixel(float64(x), float64(y), c)
+		} else {
+			if y < ScreenHeight/2 {
+				r.SetActualPixel(float64(x), float64(y), skyColor)
 			} else {
-				if y < ScreenHeight/2 {
-					r.SetActualPixel(float64(x), float64(y), skyColor)
-				} else {
-					r.SetActualPixel(float64(x), float64(y), grassColor)
-				}
+				r.SetActualPixel(float64(x), float64(y), grassColor)
 			}
 		}
 	}
